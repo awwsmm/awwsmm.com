d562fb49969a2486897958c5038122cbf1da3c49
Andrew <aww@awwsmm.com>
2023-08-01 15:43:09 -0400

    fix lint-and-fix, update dependencies (#234)

--- lib/model/tag/TagData.ts
+++ lib/model/tag/TagData.ts
@@ -4,16 +4,16 @@
 export default class TagData {
   name: string;
   description: {
-    short: string,
-    long: string
-  }
+    short: string;
+    long: string;
+  };
 
   constructor(
     name: string,
     description: {
-      short: string,
-      long: string
-    }
+      short: string;
+      long: string;
+    },
   ) {
     this.name = name;
     this.description = description;
--- lib/utils/TagUtils.ts
+++ lib/utils/TagUtils.ts
@@ -1,7 +1,7 @@
-import FileUtils from "./FileUtils";
-import PostUtils from "./PostUtils";
-import ProjectUtils from "./ProjectUtils";
-import TagData from "../model/tag/TagData";
+import FileUtils from './FileUtils';
+import PostUtils from './PostUtils';
+import ProjectUtils from './ProjectUtils';
+import TagData from '../model/tag/TagData';
 
 /**
  * Helper methods for reading and processing hashtags on content.
@@ -27,14 +27,13 @@ export default abstract class TagUtils {
 
   // check that no tags are misspelled, or missing a description
   private static validateTags(): void {
-
     // read all tags defined in the JSON file
-    const jsonTagStrings = TagUtils.getFromJSON().map(meta => meta.name);
+    const jsonTagStrings = TagUtils.getFromJSON().map((meta) => meta.name);
 
     function validate(tag: string, filePath: string[]): void {
       if (!jsonTagStrings.includes(tag)) {
-        const jsonTagFile = TagUtils.tagFilePath().join("/");
-        throw new Error(`Tag "${tag}" in ${filePath.join("/")} not found in list of tags at ${jsonTagFile}`)
+        const jsonTagFile = TagUtils.tagFilePath().join('/');
+        throw new Error(`Tag "${tag}" in ${filePath.join('/')} not found in list of tags at ${jsonTagFile}`);
       }
     }
 
@@ -43,26 +42,26 @@ export default abstract class TagUtils {
 
     // validate hashtags in all projects
     const projectMetadata = projectNames.flatMap((project) => ProjectUtils.getMetadata(project));
-    projectMetadata.forEach(meta => {
-      meta.tags.forEach(tag => {
+    projectMetadata.forEach((meta) => {
+      meta.tags.forEach((tag) => {
         validate(tag, ProjectUtils.getMetadataFilePath(meta.project));
-      })
-    })
+      });
+    });
 
     // validate hashtags in all project log entries
     const logEntries = projectNames.flatMap((project) => ProjectUtils.getLogEntries(project));
-    logEntries.forEach(entry => {
-      entry.tags.forEach(tag => {
+    logEntries.forEach((entry) => {
+      entry.tags.forEach((tag) => {
         validate(tag, ProjectUtils.getLogFilePath(entry.project, entry.slug));
-      })
-    })
+      });
+    });
 
     // validate hashtags in all blog posts
     const posts = PostUtils.getPosts();
-    posts.forEach(post => {
-      post.tags.forEach(tag => {
+    posts.forEach((post) => {
+      post.tags.forEach((tag) => {
         validate(tag, PostUtils.getFilePath(post.slug));
-      })
-    })
+      });
+    });
   }
 }
