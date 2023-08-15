03ca4c8d0f0bd5f0049c632680706a10043b786c
Andrew <aww@awwsmm.com>
2023-08-14 22:04:04 -0400

    add light mode dark mode switch (#243)
    * add light mode / dark mode switch
    
    * add light mode / dark mode switch
    
    * add light mode / dark mode switch

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 export default function PageHeader({ isHomePage }: { isHomePage?: boolean }) {
   const title = 'Andrew Watson';
@@ -15,6 +16,44 @@ export default function PageHeader({ isHomePage }: { isHomePage?: boolean }) {
     />
   );
 
+  useEffect(() => {
+    const defaultTheme = 'dark';
+
+    const currentTheme = localStorage.getItem('theme') ?? defaultTheme;
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]') as any;
+    const switchLegend = document.getElementById('theme-switch-legend') as Element;
+
+    toggleSwitch.checked = currentTheme === 'light';
+
+    function lightMode() {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+      switchLegend.innerHTML = 'light mode';
+    }
+
+    function darkMode() {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+      switchLegend.innerHTML = 'dark mode';
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function switchTheme(e: any) {
+      if (e.target.checked) {
+        lightMode();
+      } else {
+        darkMode();
+      }
+    }
+
+    currentTheme == 'light' ? lightMode() : darkMode();
+
+    toggleSwitch.addEventListener('change', switchTheme, false);
+  });
+
   return (
     <>
       <header className="page-header-container">
@@ -26,6 +65,13 @@ export default function PageHeader({ isHomePage }: { isHomePage?: boolean }) {
             {title}
           </Link>
         )}
+        <div className="theme-switch-wrapper">
+          <label className="theme-switch">
+            <input type="checkbox" id="checkbox" />
+            <div className="slider round"></div>
+          </label>
+          <div id="theme-switch-legend"></div>
+        </div>
       </header>
     </>
   );
