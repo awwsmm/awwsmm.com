dbe53a10ee497ab6ae508819abdf66ccbb3cd9e0
Andrew William Watson <aww@awwsmm.com>
2023-08-28 21:14:26 -0400

    remove commented-out class

--- lib/utils/ProjectUtils.ts
+++ lib/utils/ProjectUtils.ts
@@ -8,30 +8,6 @@ import { parseISO } from 'date-fns';
 import Project from '../model/project/ProjectData';
 import ProjectMetadata from '../model/project/ProjectMetadata';
 
-// class GitLabCommit {
-//   id: string;
-//   // short_id: string;
-//   // title: string;
-//   author_name: string;
-//   // author_email: string;
-//   // authored_date: string;
-//   // committer_name: string;
-//   // committer_email: string;
-//   committed_date: string;
-//   // created_at: string;
-//   message: string;
-//   // parent_ids: string[];
-//   web_url: string;
-
-//   constructor(id: string, author_name: string, committed_date: string, message: string, web_url: string) {
-//     this.id = id;
-//     this.author_name = author_name;
-//     this.committed_date = committed_date;
-//     this.message = message;
-//     this.web_url = web_url;
-//   }
-// }
-
 /**
  * Helper methods for reading and processing projects.
  */
